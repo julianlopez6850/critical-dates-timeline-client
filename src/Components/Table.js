8658b054b2add56ef43499c434b499a02ed33f1c
Julian Lopez <julianlopez6850@gmail.com>
2023-05-10 16:51:05 -0400

    Updated Table Component
    - Added Open/Closed Status for Critical Dates Table, a button to allow the user to easily change this Status on each Date row, and the ability to filter by Status.
    - Added assigned colors for Dates that are Past Due (red), Today (orange), Upcoming (green), or Closed (black).
    - Added a Tooltip on the Date column header of the Table to tell the user what the colors signify.
    - Added a Tooltip on the Status column header of the Table to tell the user what the icons signify.
    - Fixed a bug causing the Status of rows to display incorrectly when changing the Status filter due to bad key assignment (index) of each row.

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,15 +1,53 @@
-import { Divider, HStack, Text, VStack } from "@chakra-ui/react";
+import { Box, Divider, HStack, Text, Tooltip, VStack } from "@chakra-ui/react";
 import TableRow from "./TableRow";
+import { InfoIcon, LockIcon, UnlockIcon } from '@chakra-ui/icons';
 
 const DatesTable = (props) => {
     
     const columnHeaders = [
-        {text:'Date', w:'110px'},
+        {text: <Tooltip borderRadius='5px' mt='-5px' label={
+                <VStack>
+                    <HStack w='full'>
+                        <Box w='10px' h='10px' mb='-3px' bgColor='red'></Box>
+                        <Text>: Past Due</Text>
+                    </HStack>
+                    <HStack w='full'>
+                        <Box w='10px' h='10px' mb='-3px' bgColor='orange'></Box>
+                        <Text>: Today</Text>
+                    </HStack>
+                    <HStack w='full'>
+                        <Box w='10px' h='10px' mb='-3px' bgColor='green'></Box>
+                        <Text>: Upcoming</Text>
+                    </HStack>
+                    <HStack w='full'>
+                        <Box w='10px' h='10px' mb='-3px' bgColor='black'></Box>
+                        <Text>: Completed</Text>
+                    </HStack>
+                </VStack>
+            }>
+                <span>Date <InfoIcon/></span>
+            </Tooltip>, 
+        w:'110px'},
         {text:'File No.', w:'66px'},
         {text:'Event', w:'98px'},
         {text:'Buyer', w:'242px'},
         {text:'Seller', w:'242px'},
         {text:'Address', w:'242px'},
+        {text: <Tooltip borderRadius='5px' mt='-5px' label={
+                <VStack>
+                    <HStack w='full'>
+                        <Text mb='1px' color='red'><LockIcon/></Text>
+                        <Text>: Completed</Text>
+                    </HStack>
+                    <HStack w='full'>
+                        <Text mb='2px'><UnlockIcon/></Text>
+                        <Text>: Ongoing</Text>
+                    </HStack>
+                </VStack>
+            }>
+                <span>Status <InfoIcon/></span>
+            </Tooltip>,
+        w:'80px'},
     ]
 
     return (
@@ -19,10 +57,10 @@ const DatesTable = (props) => {
                     {`${props.type} | ${props.when}`}
                 </Text>
 
-                <Divider w='1000px' borderColor='red' />
+                <Divider w='1060px' borderColor='red' />
 
                 {/* Table Column Headers */}
-                <HStack w='1000px' spacing='0'>
+                <HStack w='1080px' spacing='0' pl='10px'>
                     {
                         columnHeaders.map((item, index) => {
                             return <Text w={item.w} key={index}>
@@ -32,7 +70,7 @@ const DatesTable = (props) => {
                     }
                 </HStack>
 
-                <Divider w='1000px' borderColor='red' />
+                <Divider w='1060px' borderColor='red' />
             </VStack>
 
             <VStack>
@@ -40,8 +78,8 @@ const DatesTable = (props) => {
                     props.dates.length > 0 ? props.dates.map((item, index) => {
                         return (
                             <TableRow
-                                key={index}
-                                file={item}
+                                key={item.date + item.prefix + item.type}
+                                dateInfo={item}
                             />
                         )
                     }) : (
--- src/Components/TableRow.js
+++ src/Components/TableRow.js
@@ -1,39 +1,86 @@
 import React, { useEffect, useState, Fragment } from "react"
-import { Box, Divider, HStack, Text, Tooltip, useDisclosure } from "@chakra-ui/react"
+import { Box, Button, Divider, HStack, Text, Tooltip, useDisclosure } from "@chakra-ui/react"
 import { EditFile } from "./EditFile";
+import { CheckIcon, CloseIcon, LockIcon, UnlockIcon } from '@chakra-ui/icons';
+import axios from 'axios';
 
 
 const TableRow = (props) => {
     
     const { isOpen, onOpen, onClose } = useDisclosure();
 
+    const [isClosed, setIsClosed] = useState(props.dateInfo.isClosed);
+    const [dateColor, setDateColor] = useState('');
+
+    // Update the isClosed property of date in database whenever the value is updated by the user &
+    // Update the color of a row's Date column depending on its status. i.e.
+    // Past Due => red
+    // Today => orange
+    // Upcoming => green
+    // Completed => black
+    useEffect(() => {
+
+        const leadingZero = (num) => {
+            if(num < 10)
+                return '0' + num;
+            return num;
+        }
+        
+        const d = new Date();
+        const todayString = `${d.getFullYear()}-${leadingZero(d.getMonth() + 1)}-${leadingZero(d.getDate())}`;
+
+        if(isClosed)
+            setDateColor('black');
+        else if(props.dateInfo.date === todayString)
+            setDateColor('orange');
+        else if(props.dateInfo.date > todayString)
+            setDateColor('green.500');
+        else
+            setDateColor('red.500');
+
+        const dateInfo = {
+            fileNumber: props.dateInfo.fileNumber,
+            type: props.dateInfo.type,
+            prefix: props.dateInfo.prefix,
+            isClosed: isClosed
+        }
+        
+        axios.put(`http://localhost:5000/dates`, dateInfo).then((response) => {
+
+        }).catch((error) => {
+            console.log('Error updating date: ' + error.message);
+        });
+
+    }, [isClosed])      
+
     return (
-        <HStack w='1000px' h='30px' borderRadius='10px' textAlign='left' bgColor='#0077cc' color='white' 
+        <HStack w='1060px' h='30px' borderRadius='10px' textAlign='left' bgColor='#0077cc' color='white'
+            _hover={{cursor:'pointer'}} 
             onClick={()=>{onOpen()}}
         >
-            <Box h='full' w='fit-content' p='10px' bgColor='black' display='flex' alignItems='center' borderRadius='10px' >
+            <Box h='full' w='fit-content' p='10px' bgColor={dateColor} display='flex' alignItems='center' borderRadius='10px' >
                 <Text w='90px' textAlign='center' fontWeight='bold'>
-                    {props.file.date}
+                    {props.dateInfo.date}
                 </Text>
             </Box>
 
             <Text w='50px' textAlign='center'>
-                {props.file.fileNumber}
+                {props.dateInfo.fileNumber}
             </Text>
 
             <Divider orientation='vertical' h='70%' />
 
             <Text w='81px'  textAlign='center'>
                 {
-                    props.file.prefix && (props.file.prefix === 'First ' && '1st ' || 
-                    props.file.prefix === 'Second ' && '2nd ' || '3rd ')
+                    props.dateInfo.prefix && (props.dateInfo.prefix === 'First ' && '1st ' || 
+                    props.dateInfo.prefix === 'Second ' && '2nd ' || '3rd ')
                 }
-                {props.file.type}
+                {props.dateInfo.type}
             </Text>
 
             {/* Display Buyer, Seller, Address w/ Tooltip in case of overflow. */}
             {
-                [props.file.buyer, props.file.seller, props.file.address].map((item, index) => {
+                [props.dateInfo.buyer, props.dateInfo.seller, props.dateInfo.address].map((item, index) => {
                     return <Fragment key={index}>
                         <Divider orientation='vertical' h='70%' />
                         <Tooltip
@@ -47,11 +94,24 @@ const TableRow = (props) => {
                     </Fragment>
                 })
             }
+            <Divider orientation='vertical' h='70%' />
+
+            <Button m='0px !important' w='59px' h='36px' bgColor='transparent'
+                _hover={{bgColor:'transparent'}}
+                onClick={(e)=>{
+                    e.stopPropagation();
+                    setIsClosed((isClosed) => !isClosed);
+                }}
+            >
+                <Text w='40px' textAlign='center'>
+                    { (isClosed) && <LockIcon color='red'/> || <UnlockIcon/> }
+                </Text>
+            </Button>
             
             <EditFile
                 onClose={onClose}
                 isOpen={isOpen}
-                fileNo={props.file.fileNumber}
+                fileNo={props.dateInfo.fileNumber}
             />
         </HStack>
     )
