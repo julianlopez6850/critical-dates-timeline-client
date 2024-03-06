4a82259a62c341e4b3b249934fed802743babfcd
Julian Lopez <julianlopez6850@gmail.com>
2024-03-06 12:00:16 -0500

    Footer Only Links to Company Site if User is Logged in on Production
    - Footer no longer includes a link to the company site in Production environment, unless the user is logged in.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { profileContext } from '../Helpers/profileContext';
 
 import {
     VStack,
@@ -8,6 +9,8 @@ import {
 } from '@chakra-ui/react';
 
 const Footer = () => {
+    const { profile } = useContext(profileContext);
+
     const currentYear = new Date().getFullYear();
     const [owners] = useState(process.env.REACT_APP_OWNER.split('|'))
     const [ownerLink] = useState(process.env.REACT_APP_OWNER_LINK.split('|'))
@@ -15,18 +18,27 @@ const Footer = () => {
     return (
         <VStack w='full' spacing='0' alignItems='center'>
             <Divider w='80%' opacity='1' borderColor='var(--navbar-seperator)'/>
-            <Text h='50px' color='var(--navbar-seperator)' fontSize='16px' display='flex' alignItems='center' whiteSpace='pre-wrap'>
+            <Text
+                h='50px'
+                display='flex'
+                alignItems='center'
+                fontSize='16px'
+                whiteSpace='pre-wrap'
+                color='var(--navbar-seperator)'
+            >
                 © {currentYear} ·
                 {owners.map((owner, index) => {
                     return <span key={owner}>
                         {' '}
-                        <Link
-                            href={ownerLink[index]}
-                            isExternal
-                        >
-                            {owner}
-                        </Link>
-                        {index === 0 && owners.length > 1 ? ' &' : ''}
+                        {(process.env.NODE_ENV !== 'production' || profile.loggedIn) ? 
+                            <Link href={ownerLink[index]} isExternal>
+                                {owner}
+                            </Link> :
+                            <span>
+                                {owner}
+                            </span>
+                        }
+                        {index === 0 && owners.length > 1 && ' &'}
                     </span>
                 })}
             </Text>
